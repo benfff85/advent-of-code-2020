7ac05966e490c6dcc59d1e9c4f7e3b1bdbe6e94d
Ben Ferenchak <ben.ferenchak@gmail.com>
2020-12-01 13:48:56 -0500

    Made InputReader generic for reuse

--- src/main/java/com/adventofcode/common/InputReader.java
+++ src/main/java/com/adventofcode/common/InputReader.java
@@ -1,29 +1,29 @@
-package com.adventofcode.day1;
+package com.adventofcode.common;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.List;
 import java.util.stream.Stream;
 
-import static java.util.Collections.emptySet;
+import static java.util.Collections.emptyList;
 import static java.util.Objects.requireNonNull;
+import static java.util.stream.Collectors.toList;
 
 @Slf4j
 @Component
 public class InputReader {
 
-    public Set<Integer> read(String file) {
+    public List<String> read(String file) {
         log.info("Reading file {}", file);
         try (Stream<String> stream = Files.lines(Paths.get(requireNonNull(getClass().getClassLoader().getResource(file)).toURI()))) {
-            return stream.map(Integer::parseInt).collect(Collectors.toSet());
+            return stream.collect(toList());
         } catch (Exception e) {
             log.warn("Error while reading file", e);
         }
-        return emptySet();
+        return emptyList();
     }
 
 }
--- src/main/java/com/adventofcode/day1/Controller.java
+++ src/main/java/com/adventofcode/day1/Controller.java
@@ -1,11 +1,13 @@
 package com.adventofcode.day1;
 
+import com.adventofcode.common.InputReader;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.Set;
 
 import static com.google.common.collect.Sets.combinations;
+import static java.util.stream.Collectors.toSet;
 
 
 @Slf4j
@@ -13,7 +15,7 @@ import static com.google.common.collect.Sets.combinations;
 public class Controller {
 
     public Controller(InputReader inputReader) {
-        Set<Integer> input = inputReader.read("puzzle-input/day-1.txt");
+        Set<Integer> input = inputReader.read("puzzle-input/day-1.txt").stream().map(Integer::parseInt).collect(toSet());
         process(input, 2);
         process(input, 3);
     }
