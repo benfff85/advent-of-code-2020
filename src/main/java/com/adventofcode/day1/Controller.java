7790d255e75c30f322bb01f2d822531688548cdc
Ben Ferenchak <ben.ferenchak@gmail.com>
2020-12-01 12:33:57 -0500

    Day 1 Solution

--- src/main/java/com/adventofcode/day1/Controller.java
+++ src/main/java/com/adventofcode/day1/Controller.java
@@ -3,33 +3,28 @@ package com.adventofcode.day1;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.util.Set;
+
+import static com.google.common.collect.Sets.combinations;
+
 
 @Slf4j
 @Component
 public class Controller {
 
     public Controller(InputReader inputReader) {
+        Set<Integer> input = inputReader.read("puzzle-input/day-1.txt");
+        process(input, 2);
+        process(input, 3);
+    }
 
-        ListPairIterator pairIterator = new ListPairIterator(inputReader.read("puzzle-input/day-1.txt"));
-        IntPair pair;
-        while (pairIterator.hasNext()) {
-            pair = pairIterator.next();
-            if (pair.getA() + pair.getB() == 2020) {
-                log.info("Pair {}", pair);
-                log.info("Product {}", pair.getA() * pair.getB());
+    private void process(Set<Integer> input, Integer size) {
+        for (Set<Integer> combination : combinations(input, size)) {
+            if (combination.stream().mapToInt(Integer::intValue).sum() == 2020) {
+                log.info("Combination {}", combination);
+                log.info("Product {}", combination.stream().reduce(1, (a, b) -> a * b));
             }
         }
-
-        ListTripleIterator tripleIterator = new ListTripleIterator(inputReader.read("puzzle-input/day-1.txt"));
-        IntTriple triple;
-        while (tripleIterator.hasNext()) {
-            triple = tripleIterator.next();
-            if (triple.getA() + triple.getB() + triple.getC() == 2020) {
-                log.info("Triple {}", triple);
-                log.info("Product {}", triple.getA() * triple.getB() * triple.getC());
-            }
-        }
-
     }
 
 }
--- src/main/java/com/adventofcode/day1/InputReader.java
+++ src/main/java/com/adventofcode/day1/InputReader.java
@@ -5,25 +5,25 @@ import org.springframework.stereotype.Component;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static java.util.Collections.emptyList;
+import static java.util.Collections.emptySet;
 import static java.util.Objects.requireNonNull;
 
 @Slf4j
 @Component
 public class InputReader {
 
-    public List<Integer> read(String file) {
+    public Set<Integer> read(String file) {
         log.info("Reading file {}", file);
         try (Stream<String> stream = Files.lines(Paths.get(requireNonNull(getClass().getClassLoader().getResource(file)).toURI()))) {
-            return stream.map(Integer::parseInt).collect(Collectors.toList());
+            return stream.map(Integer::parseInt).collect(Collectors.toSet());
         } catch (Exception e) {
             log.warn("Error while reading file", e);
         }
-        return emptyList();
+        return emptySet();
     }
 
 }
