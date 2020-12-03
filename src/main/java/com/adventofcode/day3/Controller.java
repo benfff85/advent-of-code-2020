890895d2fc9991ceb1ab4d4ff47abeec4364cf62
Ben Ferenchak <ben.ferenchak@gmail.com>
2020-12-03 01:27:32 -0500

    Minor cleanup

--- src/main/java/com/adventofcode/day3/Controller.java
+++ src/main/java/com/adventofcode/day3/Controller.java
@@ -4,7 +4,6 @@ import com.adventofcode.common.InputReader;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -13,30 +12,22 @@ import static java.util.stream.Collectors.toList;
 @Component("controller-day-3")
 public class Controller {
 
+    private final List<Row> treeMap;
 
     public Controller(InputReader inputReader) {
-        List<String> input = inputReader.read("puzzle-input/day-3.txt");
-        List<Row> treeMap = input.stream().map(Row::new).collect(toList());
-        List<Long> treeCollisionsPerRun = new ArrayList<>();
-        treeCollisionsPerRun.add(process(1, 1, treeMap));
-        treeCollisionsPerRun.add(process(1, 3, treeMap));
-        treeCollisionsPerRun.add(process(1, 5, treeMap));
-        treeCollisionsPerRun.add(process(1, 7, treeMap));
-        treeCollisionsPerRun.add(process(2, 1, treeMap));
-        log.info("Total Tree Count: {}", treeCollisionsPerRun.stream().reduce(1L, (a, b) -> a * b));
+        treeMap = inputReader.read("puzzle-input/day-3.txt").stream().map(Row::new).collect(toList());
+        log.info("Total Tree Count: {}", process(1, 1) * process(1, 3) * process(1, 5) * process(1, 7) * process(2, 1));
     }
 
-    private Long process(int verticalSpeed, int horizontalSpeed, List<Row> treeMap) {
+    private Long process(int verticalSpeed, int horizontalSpeed) {
         int treeCount = 0;
         int horizontalIndex = 0;
-        int verticalIndex = 0;
 
-        while (verticalIndex < treeMap.size()) {
+        for (int verticalIndex = 0; verticalIndex < treeMap.size(); verticalIndex += verticalSpeed) {
             if (treeMap.get(verticalIndex).isTree(horizontalIndex)) {
                 treeCount++;
             }
             horizontalIndex += horizontalSpeed;
-            verticalIndex += verticalSpeed;
         }
         log.info("Tree Count: {}", treeCount);
         return (long) treeCount;
