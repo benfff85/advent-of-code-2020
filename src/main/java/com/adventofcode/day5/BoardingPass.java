c9a8bbf86b38caf97454d8864bc4cbe2aea6f479
Ben Ferenchak <ben.ferenchak@gmail.com>
2020-12-05 01:22:27 -0500

    Simplified BoardingPass construction

--- src/main/java/com/adventofcode/day5/BoardingPass.java
+++ src/main/java/com/adventofcode/day5/BoardingPass.java
@@ -2,37 +2,18 @@ package com.adventofcode.day5;
 
 import lombok.Data;
 
+import static java.lang.Integer.parseInt;
+
 @Data
 public class BoardingPass {
 
-    private long row = 0;
-    private long column = 0;
-    private long seadId;
+    private int row;
+    private int column;
+    private int seadId;
 
     public BoardingPass(String input) {
-        if('B' == input.charAt(0))
-            row +=64;
-        if('B' == input.charAt(1))
-            row +=32;
-        if('B' == input.charAt(2))
-            row +=16;
-        if('B' == input.charAt(3))
-            row +=8;
-        if('B' == input.charAt(4))
-            row +=4;
-        if('B' == input.charAt(5))
-            row +=2;
-        if('B' == input.charAt(6))
-            row +=1;
-
-        if('R' == input.charAt(7))
-            column +=4;
-        if('R' == input.charAt(8))
-            column +=2;
-        if('R' == input.charAt(9))
-            column +=1;
-
+        row = parseInt(input.substring(0, 7).replace('B', '1').replace('F', '0'), 2);
+        column = parseInt(input.substring(7, 10).replace('R', '1').replace('L', '0'), 2);
         seadId = (row * 8) + column;
-
     }
 }
--- src/main/java/com/adventofcode/day5/Controller.java
+++ src/main/java/com/adventofcode/day5/Controller.java
@@ -15,9 +15,9 @@ public class Controller {
         List<String> input = inputReader.read("puzzle-input/day-5.txt");
         input.stream().map(BoardingPass::new).map(BoardingPass::getSeadId).max(Long::compare).ifPresent(rec -> log.info("Max seat number: {}", rec));
 
-        List<Long> seatIds = input.stream().map(BoardingPass::new).map(BoardingPass::getSeadId).sorted().collect(Collectors.toList());
-        long previousSeatId = 69L;
-        for (Long seatId : seatIds) {
+        List<Integer> seatIds = input.stream().map(BoardingPass::new).map(BoardingPass::getSeadId).sorted().collect(Collectors.toList());
+        int previousSeatId = 69;
+        for (Integer seatId : seatIds) {
             if (previousSeatId != (seatId - 1)) {
                 log.info("My seat number: {}", seatId - 1);
             }
